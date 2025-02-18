76a5a88635835c14d1638f210a1396d859a338dd
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2025-02-18 17:18:05 +0530

    new update

--- nodejs-backend.js
+++ nodejs-backend.js
@@ -261,3 +261,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// Vercel requires `module.exports`
+module.exports = app;
